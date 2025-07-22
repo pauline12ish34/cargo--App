0a435529b6b1dc93de6b45ec06120f677f76d4ad
pauline12ish34 <paulineishimwe12@gmail.com>
2025-07-22 13:19:09 +0200

    added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:cargo_app/screens/onboardingScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
