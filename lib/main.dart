c49de5df9d8aa81a3b3e56a8ba7043924a9be792
Ange-Constance <constance.nimuhire@gmail.com>
2025-07-26 10:55:14 +0200

    changed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:cargo_app/screens/onboardingScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/forget_password_screen.dart';
+import 'screens/verify_code_screen.dart';
+import 'screens/splash_screen.dart';
+import 'screens/password_reset_confirmation.dart';
+import 'screens/set_new_password_screen.dart';
 
 
 void main() async {
@@ -15,13 +23,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Routing App',
+            title: 'CargoLink',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        useMaterial3: true,
+      ),
+      initialRoute: '/splash',
       routes: {
-        '/': (context) => Onboarding(),
-        '/onboarding': (context) => Onboarding(),
-      },
+  '/splash': (context) => const SplashScreen(),
+  '/': (context) => WelcomeScreen(),
+  '/login': (context) => LoginScreen(),
+  '/signup': (context) => SignupScreen(),
+  '/forgot-password': (context) => const ForgetPasswordScreen(),
+  '/verify-code': (context) => const VerifyCodeScreen(),
+  '/password-reset-confirmation': (context) => const PasswordResetConfirmation(),
+  '/set-new-password': (context) => const SetNewPasswordScreen(),
+},
     );
   }
 }
