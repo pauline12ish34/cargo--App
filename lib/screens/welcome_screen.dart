b846513321a80c6ddad8c6997c0b0b970361d19a
Ange-Constance <constance.nimuhire@gmail.com>
2025-07-26 10:38:40 +0200

    added welcoming page

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/welcome.png", height: 250),
+            const SizedBox(height: 30),
+            const Text(
+              "Welcome to CargoLink",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text("Fill the form to continue",
+                style: TextStyle(color: Colors.grey)),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/signup'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                backgroundColor: const Color(0xFF08914D),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+              ),
+              child: const Text("CREATE ACCOUNT"),
+            ),
+            const SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: () => Navigator.pushNamed(context, '/login'),
+              style: OutlinedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                side: BorderSide(color: const Color(0xFF08914D)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+              ),
+              child: const Text("LOGIN",
+                  style: TextStyle(color: Color(0xFF08914D))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
