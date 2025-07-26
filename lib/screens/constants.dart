493749084e1a9a6fa3bd686973f2810899194e2a
Ange-Constance <constance.nimuhire@gmail.com>
2025-07-26 11:03:16 +0200

    added constants

---
+++ lib/screens/constants.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const Color primaryGreen = Color(0xFF008853);
+const Color lightGreen = Color(0xFFC5E7DF);
+const Color inputBg = Color(0xFFF1F7FA);
+const Color borderGray = Color(0xFFCBD5E1);
+const Color textDark = Color(0xFF1E293B);
+const Color textGray = Color(0xFF64748B);
+const Color resendLinkBlue = Color(0xFF0E4C92);
+
+const double borderRadius = 12.0;
+
+const TextStyle titleStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+  color: textDark,
+);
+
+const TextStyle subtitleStyle = TextStyle(
+  fontSize: 14,
+  color: textGray,
+);
+
+const TextStyle buttonTextStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const TextStyle verifyTextStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+  color: textDark,
+);
